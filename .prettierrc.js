b266c98a5a9f845d8f645ba0dbff652673d52931
Nykolai Topchyi <nodeTempest@gmail.com>
2019-12-26 21:55:25 +0200

    initial

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 4,
+    semi: false,
+    singleQuote: false,
+    trailingComma: "es5",
+    useTabs: false,
+    jsxSingleQuote: false,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: "avoid",
+    htmlWhitespaceSensitivity: "css",
+    endOfLine: "auto",
+}
