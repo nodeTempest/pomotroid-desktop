6d2d1afe9ae2258ff6e9f86408b2886940dab920
Nykolai Topchyi <nodeTempest@gmail.com>
2020-04-13 14:24:46 +0300

    [Feature] add pallete theme

---
+++ src/styled/theme.ts
@@ -0,0 +1,34 @@
+const palette = {
+    bg: {
+        dark: "#3c4356",
+        light: "#2f384b",
+    },
+    stages: {
+        work: "#fe4d4c",
+        sbreak: "#05eb8b",
+        lbreak: "#0bbcda",
+    },
+    text: {
+        light: "#f5f1ea",
+        dark: "#848b98",
+        highlight: "#05eb8b",
+    },
+    button: {
+        fg: {
+            light: "#f5f1ea",
+            dark: "#848b98",
+            hover: "#fe4d4c",
+        },
+        bg: {
+            dark: "#3c4356",
+            light: "#2f384b",
+            hover: "#2f384b",
+        },
+    },
+}
+
+const theme = {
+    palette,
+}
+
+export default theme
