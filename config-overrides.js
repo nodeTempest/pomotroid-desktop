d15b2302ab7b887109aa1476737a0b342a561740
Nykolai Topchyi <nodeTempest@gmail.com>
2020-04-14 01:15:17 +0300

    [Feature] add relative import possibility

---
+++ config-overrides.js
@@ -0,0 +1,15 @@
+const path = require("path")
+const { override, addWebpackAlias } = require("customize-cra")
+
+const resolvePath = src => path.resolve(__dirname, src)
+
+module.exports = override(
+    addWebpackAlias({
+        "@assets": resolvePath("src/assets"),
+        "@components": resolvePath("src/components"),
+        "@constants": resolvePath("src/constants"),
+        "@custom_modules": resolvePath("src/custom_modules"),
+        "@state": resolvePath("src/state"),
+        "@styled": resolvePath("src/styled"),
+    })
+)
