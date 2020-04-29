ea18a099d78fcd4f0bf2ec73e556fc11522484cd
Nykolai Topchyi <nodeTempest@gmail.com>
2020-04-29 23:27:03 +0300

    [Refactor] improve createReducer typing

--- src/state/common.ts
+++ src/state/common.ts
@@ -1,13 +1,13 @@
-import { AnyAction } from "redux"
+import { Reducer, AnyAction } from "redux"
 
-export const createReducer = <S, A extends AnyAction>(
+type ReducerMap<S extends any, A extends AnyAction> = {
+    [P in A["type"]]?: A extends { type: P }
+        ? (state: S, payload: A["payload"]) => S
+        : never
+}
+
+export const createReducer = <S extends any, A extends AnyAction>(
     initialState: S,
-    reducerMap: {
-        [P in A["type"]]?: A extends { type: P }
-            ? (state: S, payload: A["payload"]) => S
-            : never
-    }
-): ((state: S | undefined, action: A) => S) => (
-    state = initialState,
-    { type, payload }
-) => (reducerMap[type] ? reducerMap[type](state, payload) : state)
+    reducerMap: ReducerMap<S, A>
+): Reducer<S, A> => (state = initialState, { type, payload }) =>
+    reducerMap[type] ? reducerMap[type](state, payload) : state
