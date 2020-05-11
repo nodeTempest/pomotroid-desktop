10bc2197a49a2069dd037045abf792c3d5d90ba1
Nykolai Topchyi <nodeTempest@gmail.com>
2020-05-11 22:52:51 +0300

    [Feature] add logo.svg and apply to info page

--- src/pages/InfoPage/InfoPage.tsx
+++ src/pages/InfoPage/InfoPage.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react"
 
 import { Box } from "@styled"
+import pomotroidLogo from "@assets/logo.svg"
 
 export const InfoPage: FunctionComponent<{}> = () => {
     return (
@@ -9,21 +10,12 @@ export const InfoPage: FunctionComponent<{}> = () => {
                 About
             </Box>
             <Box display="flex" justifyContent="center" mb={2}>
-                <svg viewBox="0 0 100 100" width="90" height="90">
-                    <circle cx="50" cy="50" r="40" fill="#2f384b" />
-                    <circle
-                        cx="50"
-                        cy="50"
-                        r="24"
-                        fill="none"
-                        stroke="#fe4d4c"
-                        strokeWidth="13"
-                    />
-                </svg>
+                <img src={pomotroidLogo} width="90" height="90" />
             </Box>
             <Box fontSize={25} textAlign="center" color="text.highlight" mb={3}>
                 Pomotroid
             </Box>
+
             <Box textAlign="center" mb={2}>
                 Version 0.1.0
             </Box>
