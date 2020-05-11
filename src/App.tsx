42321ec9de90d2d29fecbfd13fd7d253ffb97ccf
Nykolai Topchyi <nodeTempest@gmail.com>
2020-05-12 02:02:23 +0300

    [Feature] add slide animation for menu page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
-import * as React from "react"
-import { Route, Redirect, Switch } from "react-router-dom"
+import React, { FunctionComponent } from "react"
+import { Route, Redirect, Switch, useLocation } from "react-router-dom"
 
 import { routes } from "@constants"
 import { AppPage, MenuPage } from "@pages"
 import { AppLayout } from "@layouts"
 import { AppContainer } from "./AppContainer"
 
-export const App = () => {
+const AppEntry: FunctionComponent<{}> = () => {
+    const location = useLocation()
     return (
-        <AppContainer>
-            <AppLayout>
-                <Switch>
-                    <Redirect exact from="/" to={routes.app.name} />
-                    <Route path={routes.app.name} component={AppPage} />
-                    <Route path={routes.menu.name} component={MenuPage} />
-                </Switch>
-            </AppLayout>
-        </AppContainer>
+        <AppLayout>
+            <Switch location={location}>
+                <Redirect exact from="/" to={routes.app.name} />
+                <Route path={routes.app.name} component={AppPage} />
+                <Route path={routes.menu.name} component={MenuPage} />
+            </Switch>
+        </AppLayout>
     )
 }
+
+export const App: FunctionComponent<{}> = () => (
+    <AppContainer>
+        <AppEntry />
+    </AppContainer>
+)
--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,14 +1,77 @@
-import * as React from "react"
-import { FunctionComponent } from "react"
+import React, { FunctionComponent } from "react"
+import styled from "styled-components"
+import { useLocation } from "react-router-dom"
+import { TransitionGroup, CSSTransition } from "react-transition-group"
 
 import { NavBar } from "@layouts/NavBar"
 import { Box } from "@styled/Box"
+import { getLevelName } from "@utils"
+import { routes } from "@constants"
+
+interface ITransitionContainer {
+    onAppPage: boolean
+}
+
+// need to freeze app page for transitions
+// so when app page enters transition is immediate i.e.
+// its duration is 0
+// and when app page exits transition is delayed
+const TransitionContainer = styled.div<ITransitionContainer>`
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    transition: all ${props => (props.onAppPage ? 0 : 250)}ms;
+
+    &.slide-enter {
+        transform: translateX(-100%);
+    }
+    &.slide-enter-active {
+        transform: translateX(0%);
+    }
+    &.slide-exit {
+        transform: translateX(0%);
+        transition-delay: ${props => (props.onAppPage ? 250 : 0)}ms;
+    }
+    &.slide-exit-active {
+        transform: translateX(-100%);
+    }
+`
+
+export const SlideTransition: FunctionComponent<{}> = ({ children }) => {
+    const { pathname } = useLocation()
+
+    return (
+        <TransitionGroup component={null}>
+            <CSSTransition
+                classNames="slide"
+                key={getLevelName(pathname, 0)}
+                timeout={250}
+            >
+                <TransitionContainer
+                    onAppPage={pathname.includes(routes.app.name)}
+                >
+                    {children}
+                </TransitionContainer>
+            </CSSTransition>
+        </TransitionGroup>
+    )
+}
 
 export const AppLayout: FunctionComponent<{}> = ({ children }) => {
     return (
-        <Box height={480} width={360} ml={10} mt={10} bgcolor="bg.dark">
+        <Box
+            overflow="hidden"
+            height={480}
+            width={360}
+            ml={10}
+            mt={10}
+            bgcolor="bg.dark"
+            position="relative"
+        >
             <NavBar />
-            <Box height={0.86}>{children}</Box>
+            <Box height={0.86} width={1} position="relative">
+                <SlideTransition>{children}</SlideTransition>
+            </Box>
         </Box>
     )
 }
