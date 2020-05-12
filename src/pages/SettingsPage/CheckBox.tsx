225003d3a3e77b391f966a16ab3c3f449f9d442f
Nykolai Topchyi <nodeTempest@gmail.com>
2020-05-13 01:40:01 +0300

    [Refactor] organize SettingsPage

---
+++ src/pages/SettingsPage/CheckBox.tsx
@@ -0,0 +1,63 @@
+import React, { FunctionComponent } from "react"
+import styled from "styled-components"
+
+import { Box } from "@styled"
+
+interface ICheckCircle {
+    checked: boolean
+}
+
+const CheckCircle = styled.div<ICheckCircle>`
+    border-radius: 50%;
+    transition: all 250ms;
+    width: 18px;
+    height: 18px;
+
+    background-color: ${props =>
+        props.checked && props.theme.palette.button.fg.hover};
+
+    border: 2px solid
+        ${props =>
+            props.checked
+                ? props.theme.palette.button.fg.hover
+                : props.theme.palette.button.fg.dark};
+
+    *:hover > & {
+        border: 2px solid ${props => props.theme.palette.button.fg.hover};
+    }
+`
+
+interface ICheckBoxProps {
+    checked: boolean
+    text: string
+    onChange?: (checked: boolean) => void
+}
+
+export const CheckBox: FunctionComponent<ICheckBoxProps> = ({
+    checked,
+    onChange,
+    text,
+}) => {
+    return (
+        <Box
+            onClick={() => {
+                if (onChange) {
+                    onChange(!checked)
+                }
+            }}
+            position="relative"
+            bgcolor="bg.dark"
+            borderRadius={4}
+            display="flex"
+            justifyContent="space-between"
+            p={3}
+            alignItems="center"
+            mb={3}
+        >
+            <Box color="text.dark" fontSize={14}>
+                {text}
+            </Box>
+            <CheckCircle checked={checked} />
+        </Box>
+    )
+}
