35427ee88991351ea3d469b33423ff57c4dda24d
Nykolai Topchyi <nodeTempest@gmail.com>
2020-05-22 20:28:00 +0300

    [Feature] add utils to app state

--- src/state/app/utils.ts
+++ src/state/app/utils.ts
@@ -1,9 +1,32 @@
 import * as R from "ramda"
+import { createSelector } from "@reduxjs/toolkit"
 
-import { stagesType } from "./slice"
+import { stagesType, IApp, IDurations } from "./slice"
 
 export const createStagesPattern = R.compose(
     R.update<stagesType>(-1, "lbreak"),
     R.flatten,
     R.repeat<stagesType[]>(["work", "sbreak"])
+)
+
+export const getCurrentStage = createSelector<
+    IApp,
+    stagesType[],
+    number,
+    stagesType
+>(
+    app => app.stagesPattern,
+    app => app.currentStageIndex,
+    (pattern, index) => pattern[index]
+)
+
+export const getCurrentStageDuration = createSelector<
+    IApp,
+    stagesType,
+    IDurations,
+    IDurations[stagesType]
+>(
+    getCurrentStage,
+    app => app.durations,
+    (stage, durations) => durations[stage]
 )
