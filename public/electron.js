10c3fd1614258c7144f2b024b0377815a55c08f2
Nykolai Topchyi <nodeTempest@gmail.com>
2020-05-28 17:24:00 +0000

    [Feature] add refresh window shortcut

--- public/electron.js
+++ public/electron.js
@@ -1,14 +1,13 @@
 const electron = require("electron")
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
-
 const path = require("path")
 const isDev = require("electron-is-dev")
 
-let mainWindow
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+const globalShortcut = electron.globalShortcut
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
+    const mainWindow = new BrowserWindow({
         width: 360,
         height: 480,
         frame: false,
@@ -17,14 +16,18 @@ function createWindow() {
             nodeIntegration: true,
         },
     })
+
     mainWindow.loadURL(
         isDev
             ? "http://localhost:3000"
             : `file://${path.join(__dirname, "../build/index.html")}`
     )
+
     mainWindow.on("closed", () => {
         mainWindow = null
     })
+
+    globalShortcut.register("f5", () => mainWindow.reload())
 }
 
 app.on("ready", createWindow)
